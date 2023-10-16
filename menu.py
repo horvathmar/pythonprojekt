794c03a30f88ce92fa602cc1e1129c7d59ef3400
Farkasadamattila <farkas.adam.attila@students.jedlik.eu>
2023-10-16 13:56:20 +0200

    kevés(nagyon) haladás

---
+++ menu.py
@@ -0,0 +1,17 @@
+from vagas import vagas
+from kulcs import kulcs
+from asas import asas
+
+def fomenu():
+    print(f'Játékos neve: {nev} ')
+    print()
+    print(f'Egészségi szinted: {egeszseg}')
+    print(f'Energiaszinted: {energia} ')
+    print(f'Pénzed: {penz} ')
+    print(f'Lebukási esélyed: {lebukas}%')
+
+def mellekmenu():
+    print(f'Eszköztárad: ')
+    print(f'Szökésedhez ({}) szükséges eszközök: ')
+    print(f'Kivégézesig fentmaradó idő: {} nap')
+    print('')
