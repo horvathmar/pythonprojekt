77c51921c16ab9e1f3b9ce4084f9a0637914ff62
Horvath Mark <horvath.mark@students.jedlik.eu>
2023-10-16 13:21:40 +0200

    aldg

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from kulcs import kulcs
 from asas import asas
 
 def main():
-    print('Üdvözöllek a játékban! Folyamatos választási lehetőségek, és adott esetben kihívások is lesznek a játék során. A játékban láthatjuk a szökevényünk egészségi szintjét, a pénzt, a szökéstől eltelt időt, lebukási esély százalékban és az Energia szintünket is láthatjuk. Sok esetben a játékosnak kell megadnia a kért információkat.')
+    print('Üdvözöllek a játékban! Folyamatos választási lehetőségek, és adott esetben kihívások is lesznek a játék során. A játékban láthatjuk a szökevényünk egészségi szintjét, a pénzt, kivégzésig fennmaradó idő, lebukási esély százalékban és az Energia szintünket is láthatjuk. Sok esetben a játékosnak kell megadnia a kért információkat.')
     nev = input('Adja meg a szökevény nevét: ')
     print(f'Cél: {nev}-et kiszöktetni a börtönből')
     print(f'Jelenleg {nev} egy börtöncellában fekszik az ágyán és azon gondolkozik, hogy hogyan tudna kiszabadúlni a börtönből')
