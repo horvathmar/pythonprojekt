84ebee49b9db6ceaf8fed0c5456b09ab5006661a
Horvath Daniel <horvath.daniel1@students.jedlik.eu>
2023-10-17 09:13:19 +0200

    haladas

--- vagas.py
+++ vagas.py
@@ -7,8 +7,20 @@ def vago(nev):
         valasztott_ember = int(input('Válassza ki, hogy ki csempéssze be neki a vágóeszközt! Írjon be 1-est ha a testvére és 2-est, ha egy barátja. '))
         if valasztott_ember == 1:
             print(f'A tesvérében bízik {nev}, ezért jól választottál!')
+            megbeszeles = int(input('Válaszd ki, hogy hol kérje meg a tesvérét, 1-est ha telefonon és 2-est, ha levélben.'))
+            if megbeszeles == 1:
+                print('Reménykedniük kell, hogy ne hallgassák le a telefont.')
+            elif megbeszeles == 2:
+                print('Ez egy biztonságosabb mód, de lassabban ér oda az üzenet.')
+        elif valasztott_ember == 2:
+            print(f'{nev}-nek reménykednie kell, hogy a barátja, akiben ő bízik nem adja fel a rendőröknek.') 
+            print(f'Szerencéje van, mivel a barátja nem adja fel, és benne van hogy segít {nev}-nek.')
+            megbeszeles2 = int(input('Válaszd ki, hogy hol kérje meg a tesvérét, 1-est ha telefonon és 2-est, ha levélben.'))
+            if megbeszeles2 == 1:
+                print('Reménykedniük kell, hogy ne hallgassák le a telefont.')
+            elif megbeszeles2 == 2:
+                print('Ez egy biztonságosabb mód, de lassabban ér oda az üzenet.')
 
     elif elso == 2:
         print('Így sokkal kisebb a lebukási esély, de pénzt kell szereznie az elitéltnek!')
-        penzkeresetimod = int(input(f'Hogyan szerezzen {nev} pénzt? Írjon be 1-est ha menjen el dolgozni a börtönben, és 2-est, ha kezdjen el árulni valamit, amit más behozott neki. '))
-        
+        penzkeresetimod = int(input('Hogyan szerezzen az elitélt pénzt? Írjon be 1-est ha menjen el dolgozni a börtönben, és 2-est, ha kezdjen el árulni valamit, amit más behozott neki. '))
