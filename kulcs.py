1962b227d2efceaad5fdaef00d1d098161e70ce7
Horvath Mark <horvath.mark@students.jedlik.eu>
2023-10-17 09:13:37 +0200

    haladás

--- kulcs.py
+++ kulcs.py
@@ -1,14 +1,27 @@
 def kulcsketto(nev):
     print(f'A kulcsos szökési módot választottad {nev}-nek')
-    print(f'{nev}-nek szüksége lesz egy kulcsra, amihez szüksége van a következőkre: ')
-    print('\t A kovács közelébe kell férkőzni, és megcsináltatni a kulcsot')
-    print('\t Szüksége lesz a kulcshoz szükséges anyagokra')
+    print(f'{nev}-nek szüksége lesz kettő kulcsra, és az ahhoz szükséges anyagokra')
     print('')
-    print(f'A raboknak minden nap 16:00-18:00-ig kell dolgozniuk a műhelyben, ilyenkor {nev}-nek lehetősége lehet arra, hogy találkozzon a kováccsal, de vigyázz nehogy észre vegyék!')
+    print(f'A raboknak minden nap 16:00-18:00-ig kell dolgozniuk a műhelyben, ilyenkor {nev}-nek lehetősége lehet arra, hogy bejusson a kovácsműhelybe, de vigyázz nehogy észre vegyék!')
     print('1 óra múlva...')
     print('A rabok elindulnak dolgozni')
-    valaszelso = input(f'Szeretne {nev} elszökni a többiek közűl, és találkozni a kováccsal?, ha igen (1), ha nem (2)')
+    valaszelso = int(input(f'Szeretne {nev} elszökni a többiek közűl, és elmenni a kovácsműhelybe?, ha igen (1), ha nem (2)'))
     if valaszelso == 1:
-        print('aff')
+        print(f'{nev} észrevétlenül kilép a tömegből, és elindul a műhely felé. A következő fél órát azzal tölti, hogy információkat szerezzen a műhelyről, de vigyáznod kell, nehogy feltűnjön valakinek, hogy nem vagy ott.')
+        valaszketto = int(input(f'{nev} szeretne visszaindúlni?, ha igen (1), ha nem (2)'))
+        if valaszketto == 1:
+            print(f'Jól döntöttél, {nev} elindul vissza és sikeresen beleolvad a tömegbe')
+        elif valaszketto == 2:
+            print(f'Rosszúl döntöttél, valakinek feltűnik, hogy {nev} nincs ott')
+            print('Megkérdezik tőled, hogy hol voltál, mit válaszolsz?')
+            print('\t 1 - Eltévedett és nem talált vissza')
+            print('\t 2 - Rájött a szapora')
+            print('\t 3 - Elmondod az igazat')
+            valaszharom = int(input('A választásod: '))
+            if valaszharom == 1:
+                print('Nagyon átlátszó, az őrök gyanút fognak, de elengednek, ezt most megúsztad')
+            elif valaszharom ==2:
+                print('ifz')            
     elif valaszelso ==2:
-        print('aézhioéuz')
+        print('Keményen végig dolgoztad a napot')
+        print('Másnap...')
