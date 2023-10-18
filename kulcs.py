48bf8c6dce2815567441dad2dbd3277be4565586
Horvath Mark <horvath.mark@students.jedlik.eu>
2023-10-18 10:34:34 +0200

    haladás

--- kulcs.py
+++ kulcs.py
@@ -1,3 +1,4 @@
+from random import randint
 def kulcsketto(nev):
     print(f'A kulcsos szökési módot választottad {nev}-nek')
     print(f'{nev}-nek szüksége lesz kettő kulcsra, és az ahhoz szükséges anyagokra')
@@ -5,17 +6,17 @@ def kulcsketto(nev):
     print(f'A raboknak minden nap 16:00-18:00-ig kell dolgozniuk a műhelyben, ilyenkor {nev}-nek lehetősége lehet arra, hogy bejusson a kovácsműhelybe, de vigyázz nehogy észre vegyék!')
     print('1 óra múlva...')
     print('A rabok elindulnak dolgozni')
-    valaszelso = int(input(f'Szeretne {nev} elszökni a többiek közűl, és elmenni a kovácsműhelybe?, ha igen (1), ha nem (2)'))
+    valaszelso = int(input(f'Szeretne {nev} elszökni a többiek közűl, és elmenni a kovácsműhelybe?, ha igen (1), ha nem (2): '))
     if valaszelso == 1:
         print(f'{nev} észrevétlenül kilép a tömegből, és elindul a műhely felé. A következő fél órát azzal tölti, hogy információkat szerezzen a műhelyről, de vigyáznod kell, nehogy feltűnjön valakinek, hogy nem vagy ott.')
-        valaszketto = int(input(f'{nev} szeretne visszaindúlni?, ha igen (1), ha nem (2)'))
+        valaszketto = int(input(f'{nev} szeretne visszaindúlni?, ha igen (1), ha nem (2): '))
         if valaszketto == 1:
             print(f'Jól döntöttél, {nev} elindul vissza és sikeresen beleolvad a tömegbe')
         elif valaszketto == 2:
             print(f'Rosszúl döntöttél, valakinek feltűnik, hogy {nev} nincs ott')
             print('Megkérdezik tőled, hogy hol voltál, mit válaszolsz?')
             print('\t 1 - Eltévedett és nem talált vissza')
-            print('\t 2 - Rájött a szapora')
+            print('\t 2 - El kellett mennie WC-re')
             print('\t 3 - Elmondod az igazat')
             valaszharom = int(input('A választásod: '))
             if valaszharom == 1:
@@ -23,8 +24,115 @@ def kulcsketto(nev):
             elif valaszharom ==2:
                 print('Megérhető, nem fognak gyanút, ezt most megúszta')            
             elif valaszharom == 3:
-                print('Rossz válassz, büntetésből többet kell dolgoznia minden nap')
-                print('Nem tudták meg az eredeti célod, de legközelebb jobban vigyázz')
-    elif valaszelso ==2:
-        print('Keményen végig dolgoztad a napot')
+                print('Rossz válasz')
+                print('VÉGE!')
+                # lebukas += 100
+            
+    if valaszelso == 2 or valaszketto == 1 or valaszketto ==2:
+        print('Keményen végig dolgoztad a napot.')
         print('Másnap...')
+        valasznegy = int(input(f'Szeretne {nev} észrevétlenül anyagokat gyűjteni, vagy még jobban körűlnézni?, ha anyagokat akar gyűjteni(1), körűlnézni(2), maradni és dolgozni(3): '))
+        if valasznegy == 1:
+            print(f'A mai nap során {nev} anyagokat próbál gyűjteni')
+            print('A cellatársánál van egy bökő, ami alkalmas lehet ahhoz, hogy kulcsokat készítsen belőle, de kell egy fair trade ehhez. ')
+            print('Mit ajánlasz neki érte?')
+            print('\t 1 - A mai és a holnapi ebédet')
+            print('\t 2 - Aludhat az ágyadon egy hétig')
+            print('\t 3 - Nem ad neki semmit')
+            valaszot = int(input('A választás: '))
+            if valaszot == 1:
+                print('Egy jó ajánlat volt')
+                print('Megszerezted a bökőt')
+                valasznyolc= int(input('Szeretnél másnap elindúlni kulcsot készíteni?, ha igen(1), ha nem(2)'))
+                if valasznyolc == 1:
+                    print('Másnap...')
+                    print('Észrevétlenül elszökik')
+                    print(f'A műhelyben {nev} a gép elött áll és készíti a kulcsot, de vigyáznia kell, nehogy eltörjön, vagy elrontsa a kulcsot')
+                    e = 5
+                    f = 6
+                    eselyharom = randint(e,f)
+                    if eselyharom == e:
+                        print('Sikerül elkészítenie a kulcsot')
+                    elif eselyharom == f:
+                        print('Eltörte a kulcsot, ráadásúl élszre is veszik')
+                        print('VÉGE!')
+                        # lebukas += 100
+                elif valasznyolc == 2:
+                    print(f'A következő napon {nev} végig dolgozott.')
+                    print('Elhatározza, hogy másnap elindul és megpróbálja elkészíteni a kulcsot')
+                    print('Másnap...')
+                    print('Észrevétlenül elszökik')
+                    print(f'A műhelyben {nev} a gép elött áll és készíti a kulcsot, de vigyáznia kell, nehogy eltörjön, vagy elrontsa a kulcsot')
+                    g = 5
+                    h = 6
+                    eselynegy = randint(g,h)
+                    if eselynegy == g:
+                        print('Sikerül elkészítenie a kulcsot')
+                        valaszkilenc = int(input('Szeretne még ma elindulni és kiszabadúlni a börtönből?, ha igen(1), ha nem(2): '))
+                        if valaszkilenc == 1:
+                            i = 1
+                            j = 2
+                            eselyot = randint(i,j)
+                            if eselyot == i:
+                                print('Túl mohó volt, elkapták')
+                                print('VÉGE!')
+                                # lebukas += 100
+                            elif eselyot == j:
+                                print('Sikerül kiszabadúlnia, gratulálunk, kivitte a játék')
+                                print('NYERT!')
+                                print(f'A győztes játékos: {nev}')
+                        elif valaszkilenc == 2:
+                            k = 1
+                            l = 2
+                            eselyot = randint(k, l)
+                            if eselyot == k:
+                                print('Észreveszik és elkapták')
+                                print('VÉGE!')
+                                # lebukas += 100
+                            elif eselyot == l:
+                                print('Sikerül kiszabadúlnia, gratulálunk, kivitte a játék')
+                                print('NYERT!')
+                                print(f'A győztes játékos: {nev}')
+                    elif eselynegy == h:
+                        print('Eltörte a kulcsot, ráadásúl élszre is veszik')
+                        print('VÉGE!')
+                        # lebukas += 100
+            elif valaszot == 2:
+                print('Neki is van ágya, miért kéne neki?')
+                print('Nem szerezte meg')
+            elif valaszot == 3:
+                print('1 - Harc')
+                print('2 - Nincs harc')
+                valaszhat = int(input('Válasz: '))
+                if valaszhat == 1:
+                    print('Ma délbe harc')
+                    a = 0
+                    b = 1
+                    esely = randint(a,b)
+                    if esely == a:
+                        print('Nyert')
+                    elif esely == b:
+                        print('Éppen el tud futni, de szét verték a búráját')
+                        print('Nincs bökő')
+                elif valaszhat == 2:
+                    print('Nincs harc, de nincs bökő')
+        elif valasznegy == 2:
+            print(f'{nev} alaposabban körbe akar nézni ma.')
+            print('Négy óra múlva...')
+            print(f'A rabok elindulnak dolgozni és {nev} elhatározza, hogy megszökik a tömegből')
+            valaszhet = int(input('De még meggondolhatja magát, ha igen(1), ha nem(2): '))
+            if valaszhet == 1:
+                print('Kemény munkával telt a nap, de nem haladt a munkával')
+            elif valaszhet == 2:
+                c = 2
+                d = 3
+                eselyketto = randint(c,d)
+                if eselyketto == c:
+                    print('Észrevétlenül el tud szökni a tömegből')
+                    print('Azzal tölti a maradék időt, hogy alaposabban körűlnézzen')
+                elif eselyketto == d:
+                    print('Elkapják, mostmár tényleg nem tudja kimagyarázni')
+                    print('VÉGE!')
+                    # lebukas += 100
+        elif valasznegy == 3:
+            print('Kemény munkával telt a mai nap')
