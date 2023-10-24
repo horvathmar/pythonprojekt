fdc34e5697fb8e408d3f22b0a04d85363e49ea5e
Farkasadamattila <farkas.adam.attila@students.jedlik.eu>
2023-10-24 09:36:20 +0200

    mini változás

--- asas.py
+++ asas.py
@@ -1,7 +1,8 @@
 def aso(nev, egeszseg, energia, lebukas, penz):
     print(f'A kiásást választottad szökési módul {nev}-nek')
+    print('\n')
     print(f'Az első lépés ahhoz, hogy kiásd magad az, hogy {nev} megszerezzem egy ásót és egy posztert, amivel kiáshatja magát, majd minden nap végén eltakarhatja a haladását a poszterrel')
-    valasz_egy = int(input('3 Lehetőséged van: 1 - Lopás a raktárból, 2 - Kézi ásó Készítése, 3 - Egy külsős ember lefizetése'))
+    valasz_egy = int(input('2 Lehetőséged van: 1 - Lopás a raktárból, 2 - Kézi ásó Készítése'))
     if valasz_egy == 1:
         print(f'{nev}-nek ügyenie kell az őrökre, mivel, ha észreveszik, vagy megtalálják nála az eszközöket, vége a játéknak!')
         valasztott = int(input('Válaszd ki az egyik lehetőséget'))
@@ -10,9 +11,8 @@ def aso(nev, egeszseg, energia, lebukas, penz):
             print('A raktárból fogja lopni a dolgokat, jó választás, ámde nagy kockázattal jár majd.')
             terv = int(input('Válaszd ki, hogy mikor próbáljon bejutni oda, 1 - Nappal, 2 - Éjjel'))
             if valasztott == 1:
-                print('Éjszaka kevesebben vannak, viszont a kamerákon van éjjellátó mód is.')
-                print('Az ajtóknál őrök vannak készenlétben, de nagyobb az esélyed, viszont másnap fáradtabb leszel az akció miatt +1 nap')
-            elif valasztott == 2:
-                print('Nappal könnyebben navigálhatsz és vegyülhetsz bele a tömegbe, de ha valaki látja hogy belógsz az ajtónál, beköphetnek.')
-                print(f'Kisebb eséllyel állsz neki a küldetéshez, de már aznap el tudod kezdeni, így nem 
-                veszítesz semmi időt')
+                print('Az ajtóknál őrök vannak készenlétben, visszafordulsz')
+                print('1 - igen, 2 - nem')
+                orok = int(input('Mit teszel: '))
+                if orok == 1:
+                    print('Visszafordultál')
