1c79f6200669afc333df87a77b7d0c9e329bfd23
Farkasadamattila <farkas.adam.attila@students.jedlik.eu>
2023-10-25 14:13:26 +0200

    otthon ki kell javítani (visszafordulás rakjon vissza a 3 lehetőséghez)

--- asas.py
+++ asas.py
@@ -1,48 +1,62 @@
 from random import randint
 from safe_input import beker
 
-def aso(nev, egeszseg, energia, lebukas, penz):
-    print(f'A kiásást választottad szökési módul {nev}-nek')
+def aso(nev, egeszseg, energia, lebukas, penz,):
     print('\n')
-    print(f'Az első lépés ahhoz, hogy kiásd magad az, hogy {nev} megszerezzem egy ásót, amivel kiáshatja magátó.    ')
-    print(f'{nev}-nek ügyenie kell az őrökre, mivel, ha észreveszik, vagy megtalálják nála az eszközöket, vége a játéknak!')
-    kukac = print('2 Lehetőséged van: 1 - Lopás a raktárból, 2 - Kézi ásó Készítése')
-    valasztott1 = int(input('Válaszd ki az egyik lehetőséget'))
-    while True:
-        if valasztott1 == 1:
-            print('A raktárból fogja lopni a dolgokat, jó választás, ámde nagy kockázattal jár majd.')
-            terv = beker('Válaszd ki, hogy mikor próbáljon bejutni oda, 1 - Nappal, 2 - Éjjel: ')
-            if terv == 2:
-                print('Az ajtóknál őrök vannak készenlétben, visszafordulsz')
-                orok = beker('1 - igen, 2 - nem:')
-                if orok == 2:
-                    print('Elkaptak, vesztettél')
+    print('--------------------------------------------------------------------------------------------------------------------------------')
+    print('--------------------------------------------------------------------------------------------------------------------------------')
+    print(f'"{nev}" elhatározta, hogy ki fog ásni a börtönből, aminek végrehajtásához egy ásót kell megszereznie.')
+    print('A játék során te fogod irányítani minden választását, sok sikert!')
+    print('--------------------------------------------------------------------------------------------------------------------------------')
+    kukac = print('Egy ásó megszerzésére 2 lehetősége van: ')
+    print('\t 1 - Lopás a raktárból')
+    print('\t 2 - Kézi ásó készítése')
+    valasztott1 = int(input('Válaszd ki az egyik lehetőséget: '))
+    if valasztott1 == 1:
+        print('--------------------------------------------------------------------------------------------------------------------------------')
+        print(f'"{nev}" úgy dönt, hogy be fog szökni a börtön raktárába, ahonnan remélhetőleg ki tud majd hozni egy ásót, viszont döntenie kell, mikor próbálkozik ezzel')
+        print('\t 1 - Nappal')
+        print('\t 2 - Éjjel')
+        terv = beker('Válassz, szerinted melyik lenne a jobb választás: ')
+        if terv == 2:
+            print('--------------------------------------------------------------------------------------------------------------------------------')
+            print(f'Az ajtók mellett két őr áll, viszont "{nev}" távolról észreveszi őket, és gondolkozni kezd')
+            print('\t 1 - Visszafordul')
+            print('\t 2 - Tovább megy')
+            orok = beker('Az ő helyében te hogy cselekednél?')
+            if orok == 2:
+                print('Minél közelebb értél, egyre gyanúsbbnak tűntél, és elkaptak. VESZTETTÉL')
+                exit (0)
+            else:
+                print('Visszament a cellájába, így maradt még átgondolni a következő lépést')
+                visszaugras = 1
+        if terv == 1:
+            print('--------------------------------------------------------------------------------------------------------------------------------')
+            print('Egy rab meglátta hogy valamiben settenkedsz, elmondod neki az igazat?')
+            print('\t 1 - igen')
+            print('\t 2 - nem')
+            szembe = beker('Mi a választásod: ')
+            if szembe == 2:
+                print('Elsétáltál mellette, és bementél, megvan a zsákmány ')
+                print('Ezzel könnyedén kiástad magad pár nap alatt, szép munka')
+                exit (0)
+            else:
+                random1 = randint(1,2)
+                if random1 == 1:
+                    print('Beköpött téged, vesztettél')  
                     exit (0)
                 else:
-                    print('Visszafordultál')
-            if terv == 1:
-                print('Egy rab szembejött veled, és megkérdezte hogy tervezel e kiszökni valamikor, elmondod neki az igazsgot?')
-                szembe = beker('1 - igen, 2 - nem: ')
-                if szembe == 2:
-                    print('Elsétáltál mellette, és bementél, megvan a zsákmány ')
-                    print('Ezzel könnyedén kiástad magad pár nap alatt, szép munka')
-                    exit (0)
-                else:
-                    random1 = randint(1,2)
-                    if random1 == 1:
-                        print('Beköpött téged, vesztettél')  
+                    print('Felajánlotta neked segítségét, így gyorsabban tudsz majd végezni, hogy válaszolsz')
+                    help = beker('1 - elfogadod, 2 - nem fogadod el')
+                    if help == 1:
+                        print('Az úr segítségének köszönhetően hamarabb, illetve mindketten kijutottatok')
                         exit (0)
                     else:
-                        print('Felajánlotta neked segítségét, így gyorsabban tudsz majd végezni, hogy válaszolsz')
-                        help = beker('1 - elfogadod, 2 - nem fogadod el')
-                        if help == 1:
-                            print('Az úr segítségének köszönhetően hamarabb, illetve mindketten kijutottatok')
+                        print('Hirtelen mérges lett válaszodat hallván, és az éj leple alatt ripityára töri az ásót. Újrakezdheted az egész operációt - 1, vagy öngyilkos lehetsz - 2')
+                        melyik = beker('Válassz: ')  
+                        if melyik == 1:
+                            ads  
+                        else: 
+                            print('Öngyilkos lettél, vége a játéknak')
                             exit (0)
-                        else:
-                            print('Hirtelen mérges lett válaszodat hallván, és az éj leple alatt ripityára töri az ásót. Újrakezdheted az egész operációt - 1, vagy öngyilkos lehetsz - 2')
-                            melyik = beker('Válassz: ')  
-                            if melyik == 1:
-                              ads  
-                            else: 
-                                print('Öngyilkos lettél, vége a játéknak')
-                                exit (0)
+        return visszaugras
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from asas import aso
 from menu import fomenu
 
 def main() -> str:
+    
     # fomenu(egeszseg, nev, energia, lebukas, penz)
     egeszseg = 0
     energia = 0
@@ -12,16 +13,22 @@ def main() -> str:
     print('Üdvözöllek a játékban! Folyamatos választási lehetőségek, és adott esetben kihívások is lesznek a játék során. A játékban láthatjuk a szökevényünk egészségi szintjét, a pénzt, kivégzésig fennmaradó időt, lebukási esélyt százalékban és az Energia szintünket is láthatjuk. Sok esetben a játékosnak kell megadnia a kért információkat.')
     nev = input('Adja meg a szökevény nevét: ')
     print(f'Cél: {nev}-t kiszöktetni a börtönből')
-    print(f'Jelenleg {nev} egy börtöncellában fekszik az ágyán és azon gondolkozik, hogy hogyan tudna kiszabadúlni a börtönből')
-    print('Három ötlete van: ')
-    print('\t 1 - Csinálni álkulcsokat és kinyitni az ajtókat')
-    print('\t 2 - Szerezni egy vágóeszközt és kivágni a kerítést')
-    print('\t 3 - Szerezni egy ásót és kiásni magát')
-    valasz = int(input('A szökési mód kiválasztása: '))
-    if valasz == 1:
-        kulcsketto(nev, egeszseg, energia, lebukas, penz)
-    elif valasz == 2:
-        vago(nev, egeszseg, energia, lebukas, penz)
-    elif valasz == 3:
-        aso(nev, egeszseg, energia, lebukas, penz)
+    print('\n')
+    print(f'Jelenleg "{nev}" egy börtöncellában fekszik az ágyán és azon gondolkozik, hogy hogyan tudna kiszabadúlni a börtönből')
+    
+    visszaugras = aso(visszaugras)
+    print(visszaugras)
+    while visszaugras != 0:
+        visszaugras = 0
+        print('Három ötlete van: ')
+        print('\t 1 - Csinálni álkulcsokat és kinyitni az ajtókat')
+        print('\t 2 - Szerezni egy vágóeszközt és kivágni a kerítést')
+        print('\t 3 - Szerezni egy ásót és kiásni magát')
+        valasz = int(input('Melyik szökési módot választod: '))
+        if valasz == 1:
+            kulcsketto(nev, egeszseg, energia, lebukas, penz)
+        elif valasz == 2:
+            vago(nev, egeszseg, energia, lebukas, penz)
+        elif valasz == 3:
+            aso(nev, egeszseg, energia, lebukas, penz,)
 main()
